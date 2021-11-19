e66bbf8766400dafe573d9822f9bbf995c16f9fd
Ashwin kumar <ashwin.gps@gmail.com>
2021-11-19 20:18:48 +0530

    Added footer component

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,72 @@
+import {
+  Box,
+  chakra,
+  Stack,
+  Text,
+  VisuallyHidden,
+  Flex,
+} from "@chakra-ui/react";
+import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+import { ReactNode } from "react";
+
+const SocialButton = ({
+  children,
+  label,
+  href,
+}: {
+  children: ReactNode;
+  label: string;
+  href: string;
+}) => {
+  return (
+    <chakra.button
+      bg="blackAlpha.100"
+      rounded={"full"}
+      w={8}
+      h={8}
+      cursor={"pointer"}
+      as={"a"}
+      href={href}
+      display={"inline-flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      transition={"background 0.3s ease"}
+      _hover={{
+        bg: "blackAlpha.200",
+      }}
+    >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  );
+};
+
+const Footer = () => {
+  return (
+    <Box bg="white">
+      <Flex
+        py={4}
+        px={10}
+        spacing={4}
+        justifyContent="space-between"
+        alignItems="center"
+        boxShadow="0 0 10px rgba(0,0,0,0.1)"
+      >
+        <Text>© 2020 Tivago. All rights reserved</Text>
+        <Stack direction={"row"} spacing={6}>
+          <SocialButton label={"Twitter"} href={"#"}>
+            <FaTwitter />
+          </SocialButton>
+          <SocialButton label={"YouTube"} href={"#"}>
+            <FaYoutube />
+          </SocialButton>
+          <SocialButton label={"Instagram"} href={"#"}>
+            <FaInstagram />
+          </SocialButton>
+        </Stack>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Footer;
